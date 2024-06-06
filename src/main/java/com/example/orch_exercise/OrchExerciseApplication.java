1904bfccabe8315617a77e3d04661fecb3918695
Al0p3z <kl.angel7@gmail.com>
2024-06-05 23:41:35 -0600

    First commit

---
+++ src/main/java/com/example/orch_exercise/OrchExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.orch_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orch_exercise/OrchExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orch_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
